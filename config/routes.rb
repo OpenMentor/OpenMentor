0ae25a6b2fdcf0c369469b25067ecff7a99fdfb0
Rick Winfrey <rewinfrey@github.com>
2015-12-29 22:39:19 -0600

    Adds routes for SkillProposals

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :skills
 
+  resources :skill_proposals
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
