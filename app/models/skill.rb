9dabe0e23e48e43db6f61e38120533dba3743bfd
Rick Winfrey <rewinfrey@github.com>
2015-12-29 22:34:09 -0600

    Adds Skill.already_skill?

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,7 @@
 class Skill < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
+
+  def self.already_skill?(skill_proposal)
+    where(arel_table[:name].matches("#{skill_proposal.name}%")).any?
+  end
 end
---
+++ spec/models/skill_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Skill do
+  describe ".already_skill?" do
+    before do
+      @skill = Factories::Skill.create!
+    end
+
+    it "returns true if a skill for the given name already exists (case insensitive)" do
+      name = @skill.name.downcase
+      skill = Skill.new(name: name)
+      expect(Skill.already_skill?(skill)).to be_truthy
+    end
+
+    it "returns false if a sklil for the given name doesn't exist" do
+      skill = Skill.new(name: "New Skill")
+      expect(Skill.already_skill?(skill)).to be_falsey
+    end
+  end
+end
