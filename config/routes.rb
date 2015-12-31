ce5052fed3920f0c91d9f244b1d0e0f409ea06ac
Rick Winfrey <rewinfrey@github.com>
2015-12-30 18:22:53 -0600

    Updates routes to be explicit

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,30 @@
 Rails.application.routes.draw do
   devise_for :mentors
 
+  root 'mentors#index'
+
+  # Mentors
   get 'mentors' => 'mentors#index', as: :mentors
   get 'mentor/:id'  => 'mentors#show', as: :mentor_show
 
+  # Mentor Profile
   get 'profiles/edit' => 'mentors#edit', as: :mentor_edit
   patch 'profiles/update' => 'mentors#update', as: :mentor_update
 
-  root 'mentors#index'
+  # MentorSkills
+  get 'mentor_skills/edit' => 'mentor_skills#edit', as: :mentor_skills_edit
+  patch 'mentor_skills/update' => 'mentor_skills#update', as: :mentor_skills_update
 
-  resources :skills
+  # Skills
+  get 'skills' => 'skills#index', as: :skills
+  get 'skills/new' => 'skills#new', as: :skill_new
+  put 'skills/create' => 'skills#create', as: :skill_create
 
+  # SkillProposals
   resources :skill_proposals
+  get 'skill_proposals' => 'skill_proposals#index', as: :skill_proposals
+  get 'skill_proposals/new' => 'skill_proposals#new', as: :skill_proposal_new
+  put 'skill_proposals/create' => 'skill_proposals#create', as: :skill_proposal_create
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
