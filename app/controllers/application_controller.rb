92426d2b3ba4c7125548366716e8434b54a00c01
Rick Winfrey <rewinfrey@github.com>
2015-12-31 23:31:17 -0600

    Adds time zone around_action to always use the current_mentor's time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_mentor!, unless: :devise_controller?
+  around_action :set_time_zone, if: :current_mentor
 
   def validate_admin!
     render_404 unless current_mentor.admin?
@@ -25,5 +26,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  def set_time_zone
+    Time.use_zone(current_mentor.time_zone &block)
   end
 end
