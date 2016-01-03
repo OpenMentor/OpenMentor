e75f57897b2b608208fa13d06e3b9934d8e7a05a
Rick Winfrey <rewinfrey@github.com>
2016-01-03 17:35:51 -0600

    Adds Mentor::AvailabilitiesController#edit and update actions

--- app/controllers/mentors/availabilities_controller.rb
+++ app/controllers/mentors/availabilities_controller.rb
@@ -4,10 +4,100 @@ module Mentors
       @availabilities = mentor.availabilities_by_day
     end
 
+    def edit
+      @availabilities = current_mentor.availabilities_by_day
+    end
+
+    def update
+      remove_old_availabilities!
+      create_new_availabilities!
+      flash[:notice] = update_success_message
+      redirect_to mentor_show_path(current_mentor)
+    rescue
+      flash[:alert] = update_error_message
+      redirect_to action: "edit"
+    end
+
     private
 
     def mentor
       @mentor ||= Mentor.find(params[:id])
+    end
+
+    def availability_params
+      params.require(:availabilities).permit(
+        sunday: [],
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: [])
+    end
+
+    def update_success_message
+      "Availability successfully updated"
+    end
+
+    def update_error_message
+      "Something went wrong updating your availability, please try again"
+    end
+
+    def canonical_dates
+      {
+        :sunday => 4,
+        :monday => 5,
+        :tuesday => 6,
+        :wednesday => 7,
+        :thursday => 8,
+        :friday => 9,
+        :saturday => 10
+      }
+    end
+
+    def day_for(day)
+      canonical_dates[day.to_sym]
+    end
+
+    def remove_old_availabilities!
+      availabilities_by_day = current_mentor.availabilities_by_day
+
+      old_availabilities = current_mentor.availabilities.select do |availability|
+        day = canonical_dates.invert[availability.start.day]
+        hour = availability.start.hour
+
+        availability_params[day].nil? || !availability_params[day].include?(hour.to_s)
+      end
+
+      old_availabilities.each do |availability|
+        availability.destroy
+      end
+    end
+
+    def create_new_availabilities!
+      availabilities_by_day = current_mentor.availabilities_by_day
+
+      start_times = availability_params.each_with_object([]) do |(day, hours), collection|
+        hours.each do |hour|
+          raise unless valid_hour?(hour)
+          collection << Time.zone.local(2015, 1, day_for(day), hour, 0, 0)
+        end
+      end
+
+      start_times = start_times.reject do |start_time|
+        weekday = canonical_dates.invert[start_time.day]
+        hour = start_time.hour
+
+        availabilities_by_day[weekday] && availabilities_by_day[weekday].include?(hour)
+      end
+
+      start_times.each do |start_time|
+        Availability.create!(mentor: current_mentor, start: start_time)
+      end
+    end
+
+    def valid_hour?(hour)
+      hour.to_i && hour.to_i > 0
     end
   end
 end
--- spec/controllers/mentors/availabilities_controller_spec.rb
+++ spec/controllers/mentors/availabilities_controller_spec.rb
@@ -13,4 +13,78 @@ describe Mentors::AvailabilitiesController do
       expect(assigns(:availabilities)).to eq(@mentor.availabilities_by_day)
     end
   end
+
+  context "edit" do
+    before do
+      @mentor = Factories::Mentor.create!
+      as_mentor(@mentor) do
+        get :edit
+      end
+    end
+
+    it "assigns availabilities for the current mentor" do
+      expect(assigns(:availabilities)).to eq(@mentor.availabilities_by_day)
+    end
+  end
+
+  context "update" do
+    context "success" do
+      before do
+        @mentor = Factories::Mentor.create!
+        Time.zone = @mentor.time_zone
+        Factories::Availability.create!(mentor: @mentor, year: 2015, month: 1, day: 4, hour: 12)
+        Factories::Availability.create!(mentor: @mentor, year: 2015, month: 1, day: 5, hour: 12)
+        params = {
+          availabilities: {
+            sunday: ["10", "12"],
+            monday: ["11"],
+            tuesday: ["12"],
+            wednesday: ["13"],
+            thursday: ["14"],
+            friday: ["15"],
+            saturday: ["16"]
+          }
+        }
+        as_mentor(@mentor) do
+          patch :update, params
+        end
+      end
+
+      it "creates or removes availabilities for the given mentor" do
+        Time.zone = @mentor.time_zone
+        expect(@mentor.reload.availabilities.map(&:start).map(&:hour)).to eq [12, 10, 11, 12, 13, 14, 15, 16]
+        expect(@mentor.reload.availabilities.map(&:start).map(&:wday)).to eq [0, 0, 1, 2, 3, 4, 5, 6]
+      end
+
+      it "redirects back to the mentor show page" do
+        expect(response).to redirect_to(mentor_show_path(@mentor))
+      end
+
+      it "sets a flash notice" do
+        expect(flash[:notice]).to_not be_nil
+      end
+    end
+
+    context "error" do
+      before do
+        @mentor = Factories::Mentor.create!
+        params = {
+          availabilities: {
+            sunday: ["bad params"]
+          }
+        }
+        as_mentor(@mentor) do
+          patch :update, params
+        end
+      end
+
+      it "sets a flash alert if an error occurs" do
+        expect(flash[:alert]).to_not be_nil
+      end
+
+      it "redirects back to the edit view" do
+        expect(response).to redirect_to(availabilities_edit_path)
+      end
+    end
+  end
 end
