e580d531f4f59e623b0c717d90b70239cfdc35b8
Rick Winfrey <rewinfrey@github.com>
2016-01-03 10:35:50 -0600

    Removes debugging availabilities view

--- app/controllers/mentors/availabilities_controller.rb
+++ app/controllers/mentors/availabilities_controller.rb
@@ -1,11 +1,7 @@
 module Mentors
   class AvailabilitiesController < ApplicationController
     def index
-      Time.zone = mentor.time_zone
-      @original_availabilities = mentor.availabilities_by_day
-
-      Time.zone = current_mentor.time_zone
-      @availabilities = mentor.reload.availabilities_by_day
+      @availabilities = mentor.availabilities_by_day
     end
 
     private
