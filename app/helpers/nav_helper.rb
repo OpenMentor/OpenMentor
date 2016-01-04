ba6f7c165c9322197a3f89229242703bbfd9375e
Stephanie Briones <smbriones@gmail.com>
2016-01-03 21:57:41 -0600

    Adds navigation helper, updates links

---
+++ app/helpers/nav_helper.rb
@@ -0,0 +1,9 @@
+module NavHelper
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'current' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+end
