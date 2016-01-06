1e4d8e92449150b79daff15909268c02273e7668
Rick Winfrey <rewinfrey@github.com>
2016-01-05 23:28:55 -0600

    Updates routes to be consistently pluralized

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
 
   # Mentors
   get 'mentors' => 'mentors#index', as: :mentors
-  get 'mentors/:id'  => 'mentors#show', as: :mentor_show
+  get 'mentors/:id'  => 'mentors#show', as: :mentors_show
+  get 'mentors/search' => 'mentors#search', as: :mentors_search
 
   # Mentor Profile
-  get 'profiles/edit' => 'mentors#edit', as: :mentor_edit
-  patch 'profiles/update' => 'mentors#update', as: :mentor_update
+  get 'profiles/edit' => 'mentors#edit', as: :mentors_edit
+  patch 'profiles/update' => 'mentors#update', as: :mentors_update
 
   # MentorSkills
   get 'mentor_skills/edit' => 'mentor_skills#edit', as: :mentor_skills_edit
@@ -17,13 +18,13 @@ Rails.application.routes.draw do
 
   # Skills
   get 'skills' => 'skills#index', as: :skills
-  get 'skills/new' => 'skills#new', as: :skill_new
-  put 'skills/create' => 'skills#create', as: :skill_create
+  get 'skills/new' => 'skills#new', as: :skills_new
+  put 'skills/create' => 'skills#create', as: :skills_create
 
   # SkillProposals
   get 'skill_proposals' => 'skill_proposals#index', as: :skill_proposals
-  get 'skill_proposals/new' => 'skill_proposals#new', as: :skill_proposal_new
-  put 'skill_proposals/create' => 'skill_proposals#create', as: :skill_proposal_create
+  get 'skill_proposals/new' => 'skill_proposals#new', as: :skill_proposals_new
+  put 'skill_proposals/create' => 'skill_proposals#create', as: :skill_proposals_create
 
   # Availabilities (scoped to mentor)
   get 'mentors/:id/availabilities' => 'mentors/availabilities#index', as: :availabilities
