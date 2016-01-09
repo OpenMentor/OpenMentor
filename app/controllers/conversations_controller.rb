65ec7961e9f3ebe89b8ae11c3e251bab9a7ffc75
Rick Winfrey <rewinfrey@github.com>
2016-01-09 17:57:02 -0600

    Allows only one receiver for messages

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,31 +5,41 @@ class ConversationsController < ApplicationController
 
   def new
     # Require an explicit receiver for v1.
-    # Revisit so receivers can be added as part
-    # of new message creation.
-    render_404 unless receivers.any?
+    # Revisit so multiple receivers can be added as part of new message creation.
+    receiver
   end
 
   def create
-    conversation = Conversation.create!(subject: params[:subject],
-                                        mentors: (Mentor.find(params[:receiver_ids]) << current_mentor))
-
-    Message.create!(sender_id: current_mentor.id,
-                    body: params[:body],
-                    conversation: conversation,
-                    sent_at: Time.current)
-
+    conversation.save
+    message.save
+    flash[:notice] = "Your message delivered successfully"
     redirect_to action: "index"
   end
 
   def show
-    @conversation = Conversation.find(params[:id])
+    @conversation ||= Conversation.find(params[:id])
   end
 
   private
 
-  def receivers
-    receiver_ids = params[:receiver_ids] || []
-    @receivers ||= Mentor.find(receiver_ids)
+  def receiver
+    render_404 unless params[:receiver_id]
+    @receiver ||= Mentor.find(params[:receiver_id])
+  end
+
+  def conversation
+    @conversation ||= Conversation.new(
+      subject: params[:subject],
+      mentors: [receiver, current_mentor]
+    )
+  end
+
+  def message
+    @message ||= Message.new(
+      sender_id: current_mentor.id,
+      body: params[:body],
+      conversation: conversation,
+      sent_at: Time.current
+    )
   end
 end
--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -18,48 +18,51 @@ describe ConversationsController do
     before do
       @mentor = Factories::Mentor.create!
       @other_mentor = Factories::Mentor.create!
-      @other_mentor2 = Factories::Mentor.create!
     end
 
     it "assigns a receiver" do
       as_mentor(@mentor) do
-        get :new, receiver_ids: [@other_mentor.id, @other_mentor2.id]
-        expect(assigns(:receivers)).to eq([@other_mentor, @other_mentor2])
+        get :new, receiver_id: @other_mentor.id
+        expect(assigns(:receiver)).to eq(@other_mentor)
       end
     end
 
     it "returns a 404 if no receivers are specified" do
       as_mentor(@mentor) do
-        expect { get :new, receiver_ids: [] }.to raise_error(error_404)
+        expect { get :new, receiver_id: nil }.to raise_error(error_404)
       end
     end
   end
 
   context "create" do
-    before do
-      @mentor = Factories::Mentor.create!
-      @other_mentor = Factories::Mentor.create!
-      @other_mentor2 = Factories::Mentor.create!
-      @subject = "Test Subject"
-      @body = "Test body message"
-      as_mentor(@mentor) do
-        put :create, subject: @subject, body: @body, receiver_ids: [@other_mentor.id, @other_mentor2.id]
+    context "success" do
+      before do
+        @mentor = Factories::Mentor.create!
+        @other_mentor = Factories::Mentor.create!
+        @subject = "Test Subject"
+        @body = "Test body message"
+        as_mentor(@mentor) do
+          put :create, subject: @subject, body: @body, receiver_id: @other_mentor.id
+        end
       end
-    end
 
-    it "creates a new conversation for all mentors involved" do
-      expect(@mentor.conversations.count).to eq 1
-      expect(@other_mentor.conversations.count).to eq 1
-      expect(@other_mentor2.conversations.count).to eq 1
-    end
+      it "creates a new conversation for all mentors involved" do
+        expect(@mentor.conversations.count).to eq 1
+        expect(@other_mentor.conversations.count).to eq 1
+      end
 
-    it "has the correct subject" do
-      expect(@mentor.conversations.first.subject).to eq(@subject)
-    end
+      it "has the correct subject" do
+        expect(@mentor.conversations.first.subject).to eq(@subject)
+      end
 
-    it "creates the first message from the body params" do
-      expect(@mentor.messages.count).to eq(1)
-      expect(@mentor.conversations.first.messages.first.body).to eq(@body)
+      it "creates the first message from the body params" do
+        expect(@mentor.messages.count).to eq(1)
+        expect(@mentor.conversations.first.messages.first.body).to eq(@body)
+      end
+
+      it "sets a flash notice" do
+        expect(flash[:notice]).to_not be_nil
+      end
     end
   end
 end
