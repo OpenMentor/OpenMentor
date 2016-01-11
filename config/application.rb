564ce55ba5a2e1c81d32479d2374ea2456fce177
Rick Winfrey <rewinfrey@github.com>
2016-01-11 01:01:28 -0600

    Adds /lib to load path

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
